99460eac735bd449f1bbd477c52b7958df5b05c4
Andi Heinkelein <andifeind@noname-media.com>
2018-09-02 19:00:15 +0200

    Run build

--- dist/csvRead.js
+++ dist/csvRead.js
@@ -1,63 +1,53 @@
-const fs = require('fs')
-const CSVStream = require('csv-stream')
-console.log(CSVStream)
+const fs = require('fs');
+const CSVStream = require('csv-stream');
 class CSVRead {
   constructor (readStream, opts) {
-    opts = opts || {}
+    opts = opts || {};
     const options = {
       delimiter: opts.delimiter || ',',
       endLine: opts.newline || '\n',
       escapeChar: opts.quote || '"',
-      enclosedChar: opts.quote || '"'
-    }
-    const csvStream = CSVStream.createStream(options)
-    readStream.pipe(csvStream)
+      enclosedChar: opts.quote || '"',
+      columns: opts.columns || null
+    };
+    const csvStream = CSVStream.createStream(options);
+    readStream.pipe(csvStream);
+    const res = [  ];
     csvStream.on('error', (err) => {
-      console.log('ERR', err)
-    }).on('header', (cols) => {
-      console.log('COLS', cols)
+      this.__reject(err);
     }).on('data', (data) => {
-      console.log('DATA', data)
-    }).on('column', (key, val) => {
-      console.log('COL', key, val)
-    })
+      const parsedData = this.__parseFn(data, res.length);
+      if (parsedData !== null) {
+        res.push(parsedData === undefined ? data : parsedData);
+      }
+    }).on('end', (end) => {
+      this.__resolve(res);
+    });
   }
 
   static load (file, opts) {
-    const options = {
-      encoding: 'buffer'
-    }
-    const stream = fs.createReadStream(file, options)
-    return new CSVRead(stream, opts)
+    const stream = fs.createReadStream(file);
+    return new CSVRead(stream, opts);
   }
 
   static stream (readStream) {
-    return new CSVRead(readStream)
+    return new CSVRead(readStream);
   }
 
   then (fn) {
     const p = new Promise((resolve, reject) => {
-      this.__resolve = resolve
-      this.__reject = reject
-    })
-    p.then(fn)
-    return p
+      this.__resolve = resolve;
+      this.__reject = reject;
+    });
+    p.then(fn);
+    return p;
   }
 
   parse (fn) {
-    this.__parseFn = fn
-    return this
-  }
-
-  parseLine (str) {
-    let index = 0
-    while (true) {}
-    const firstChar = str.charAt(0)
-    if (this.quoteReg.test(firstChar)) {
-      const startIndex = 1
-    }
+    this.__parseFn = fn;
+    return this;
   }
 }
-module.exports.default = CSVRead
-module.exports = { CSVRead: CSVRead }
-module.exports.__esModule = true
+module.exports.default = CSVRead;
+module.exports = { CSVRead: CSVRead };
+module.exports.__esModule = true;
